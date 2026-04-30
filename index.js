a0c9324580e182c386c4d0d6027ec863894e35d9
serenity <serenitydedman@gmail.com>
2026-04-30 16:19:33 +0000

    updated index js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+let automobiles = ["car", "truck", "motorcycle"]
+
+automobiles.push("airplane", "skateboard");
+document.getElementById("push").innerHTML = automobiles;
+
+automobiles.unshift("bike", "helicopter");
+document.getElementById("unshift").innerHTML = automobiles;
+
+let automobilesCopy = '${automobiles}'
+
+automobiles.pop();
+document.getElementById("pop").innerHTML = automobiles;
+
+automobiles.shift();
+document.getElementById("shift").innerHTML = automobiles;
+
 
